93db87eb2e62623fe3dae00119d231ee773f059d
Gabriel <gmdf10@gmail.com>
2026-03-07 00:24:17 -0300

    feat: criar arquivo no fs.js funcionando

---
+++ modules/fs.js
@@ -0,0 +1,25 @@
+const { error } = require("console");
+const fs = require("fs");
+const path = require("path");
+
+// Criar uma pasta
+// fs.mkdir(path.join(__dirname, "/test"), (error) => {
+//  if (error) {
+//    return console.log("Erro", error);
+//  }
+
+//  console.log("Pasta criada com sucesso!");
+// });
+
+// Criar um arquivo
+fs.writeFile(
+  path.join(__dirname, "/test", "teste.txt"),
+  "hello node!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+
+    console.log("Arquivo criado com sucesso!");
+  },
+);
