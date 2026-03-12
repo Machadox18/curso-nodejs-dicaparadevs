d96beef9ceda8adafb6d51ddf52f181fc9bbf651
Gabriel <gmdf10@gmail.com>
2026-03-12 16:04:45 -0300

    feat: requisição de deletar usuario funcionando

--- modules/express.js
+++ modules/express.js
@@ -49,6 +49,18 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndDelete(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Rodando com express na porta ${port}!`));
