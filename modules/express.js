c160f313304c85eee19c429e63a79ab97225d458
Gabriel <gmdf10@gmail.com>
2026-03-12 15:44:39 -0300

    feat: requisição de buscar usuario por id funcionando

--- modules/express.js
+++ modules/express.js
@@ -5,16 +5,23 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/home", (req, res) => {
-  res.contentType("application/html");
-  res.status(200).send("<h1>hello world</h1>");
-});
-
 app.get("/users", async (req, res) => {
   try {
     const users = await UserModel.find({});
 
     res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findById(id);
+
+    return res.status(200).json(user);
   } catch (error) {
     return res.status(500).send(error.message);
   }
